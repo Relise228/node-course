377dfdcccc6cdc50392cf589d22852eb496010d2
Andrii Yuzvyk <andreysmit43@gmail.com>
2023-12-03 19:03:45 +0200

    update

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const helmet = require("helmet")
 const compression = require("compression")
 const morgan = require("morgan")
 const fs = require("fs")
-const https = require("https")
+// const https = require("https")
 
 const PORT = process.env.PORT || 4000
 
@@ -28,8 +28,8 @@ const store = new MongoDBStore({
 })
 const csrfProtection = csrf()
 
-const privateKey = fs.readFileSync("server.key")
-const certificate = fs.readFileSync("server.cert")
+// const privateKey = fs.readFileSync("server.key")
+// const certificate = fs.readFileSync("server.cert")
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
